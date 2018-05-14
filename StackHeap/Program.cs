638902276c9bfd3d15df7e4cc160dad048406caa
Morten Engemand <engemand@hotmail.com>
2018-05-14 15:50:14 +0200

    StackCommit

--- StackHeap/Program.cs
+++ StackHeap/Program.cs
@@ -10,6 +10,26 @@ namespace StackHeap
     {
         static void Main(string[] args)
         {
+            Person p1 = new Person();
+            p1.Navn = "a";
+            Person p2 = new Person();
+            p2.Navn = "b";
+
+            Console.WriteLine(p1.Navn);
+            Console.WriteLine(p2.Navn);
+            p1 = p2;
+
+            Console.WriteLine(p1.Navn);
+            Console.WriteLine(p2.Navn);
+            p2.Navn = "c";
+
+            Console.WriteLine(p1.Navn);
+            Console.WriteLine(p2.Navn);
         }
+    }
+
+    class Person
+    {
+        public string Navn;
     }
 }
